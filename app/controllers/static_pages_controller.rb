0510a9fe6754cc28e08c7b73142a7afcd91b3c8d
Oscar <nyjacson.yt@gmail.com>
2016-07-12 10:24:38 +0900

    change button color. add back button. add article list on home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
       @shopinfo = Shopinfo.all
       @shopbranch = Shopbranch.all
       @article_1 = Article.find_by_sql(['select * from articles where category = :cat', {cat: 'beginner'}])
+      @article_2 = Article.find_by_sql(['select * from articles where category = :cat', {cat: 'something'}])
   end
   def toku1
   end
