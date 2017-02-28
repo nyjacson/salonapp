6fc369a7d36ba8591c908f3e5178b8c4a3fd9f63
Oscar <nyjacson.yt@gmail.com>
2017-02-28 14:56:18 +0900

    update js

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -4,15 +4,19 @@ jQuery(document).ready(function($) {
 
 
     // menu fix
+    var windowWidth = $(window).width();
+    var windowSM = 743;
+    $(window).on("resize", function(){
+        windowWidth = $(window).width();
+    })
     $(window).on("scroll", function(){
-        var scrolled = $(window).scrollTop();
-        if (scrolled > 150){
-            $(".GlobalNav").addClass("GlobalNav--fixed").fadeIn();
-        } else {
-            $(".GlobalNav").removeClass("GlobalNav--fixed");
+        if (windowWidth >= windowSM) {
+            var scrolled = $(window).scrollTop();
+            if (scrolled > 250){
+                $(".GlobalNav").addClass("GlobalNav--fixed").fadeIn();
+            } else {
+                $(".GlobalNav").removeClass("GlobalNav--fixed");
+            }
         }
     })
-
-
-
 });
