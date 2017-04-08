017442f3f4fa773f48158a56e97d7eb3030f8ec4
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2017-04-08 19:29:40 +0900

    change setting for routing change.

--- app/controllers/shopbranches_controller.rb
+++ app/controllers/shopbranches_controller.rb
@@ -1,6 +1,8 @@
 class ShopbranchesController < ApplicationController
     def index
-        @shopbranch = Shopbranch.paginate(page: params[:page],per_page: 15)
+
+        @shopinfo = Shopinfo.find(params[:shopinfo_id])
+        # @shopbranch = Shopbranch.paginate(page: params[:page],per_page: 15)
         @shopbranch_csv = Shopbranch.all
         respond_to do |format|
          format.html
