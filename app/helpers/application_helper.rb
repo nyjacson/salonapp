7c4f01175a98969345dbd215e945a20211b40080
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2017-04-15 09:32:51 +0900

    replace path of ranking svg images.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,23 +10,23 @@ module ApplicationHelper
 
     def customer_rank(rb_rank)
         if rb_rank == 5
-            image_tag('/assets/star-5.svg', class: "ele-rankStar")
+            image_tag('/images/star-5.svg', class: "ele-rankStar")
         elsif rb_rank == 4.5
-            image_tag('/assets/star-4-half.svg', class: "ele-rankStar")
+            image_tag('/images/star-4-half.svg', class: "ele-rankStar")
         elsif rb_rank == 4
-            image_tag('/assets/star-4.svg', class: "ele-rankStar")
+            image_tag('/images/star-4.svg', class: "ele-rankStar")
         elsif rb_rank == 3.5
-            image_tag('/assets/star-3-half.svg', class: "ele-rankStar")
+            image_tag('/images/star-3-half.svg', class: "ele-rankStar")
         elsif rb_rank == 3
-            image_tag('/assets/star-3.svg', class: "ele-rankStar")
+            image_tag('/images/star-3.svg', class: "ele-rankStar")
         elsif rb_rank == 2.5
-            image_tag('/assets/star-2-half.svg', class: "ele-rankStar")
+            image_tag('/images/star-2-half.svg', class: "ele-rankStar")
         elsif rb_rank == 2
-            image_tag('/assets/star-2.svg', class: "ele-rankStar")
+            image_tag('/images/star-2.svg', class: "ele-rankStar")
         elsif rb_rank == 1.5
-            image_tag('/assets/star-1-half.svg', class: "ele-rankStar")
+            image_tag('/images/star-1-half.svg', class: "ele-rankStar")
         elsif rb_rank == 1
-            image_tag('/assets/star-1.svg', class: "ele-rankStar")
+            image_tag('/images/star-1.svg', class: "ele-rankStar")
         end
     end
 
