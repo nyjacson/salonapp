483725cbcfb1f7bf7c54bae10f5bfadb532ba212
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2017-05-03 08:29:26 +0900

    configure unicorn, nginx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,33 @@
 # -*- coding: utf-8 -*-
   
-  # set config
-  listen "/var/www/rails/salonapp/tmp/sockets/.unicorn.sock"
-  pid "tmp/pids/unicorn.pid"
-  worker_processes 2
-  timeout 15
-  preload_app true
-
-  ROOT = File.dirname(File.dirname(__FILE__))
-  stdout_path "#{ROOT}/log/unicorn-stdout.log"
-  stderr_path "#{ROOT}/log/unicorn-stderr.log"
-
-  before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-    old_pid = "#{server.config[:pid]}.oldbin"
-    unless old_pid == server.pid
-      begin
-        Process.kill :QUIT, File.read(old_pid).to_i
-      rescue Errno::ENOENT, Errno::ESRCH
-      end
+$worker  = 2
+$timeout = 30
+$app_dir = "/home/oscar/www/salonapp" #自分のアプリケーション名
+$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
+# set config
+worker_processes  $worker
+working_directory $app_dir
+stderr_path $std_log
+stdout_path $std_log
+timeout $timeout
+listen  $listen
+pid $pid
+# loading booster
+preload_app true
+# before starting processes
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
-
-  after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-  end
+end
+# after finishing processes
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
