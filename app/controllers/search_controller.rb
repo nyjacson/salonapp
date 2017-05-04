36dcf3d9bbd77b9daad97ad037c0d1e6b970b00b
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2017-05-04 13:36:56 +0900

    update search query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,76 +8,76 @@ class SearchController < ApplicationController
         @shopinfo = Shopinfo.all
     end
     def ginza
-        @station = Shopbranch.find_by_sql(['select * from shopbranches where station = "銀座"'])
+        @station = Shopbranch.find_by_sql(["select * from shopbranches where station = '銀座'"])
         @shopinfo = Shopinfo.all
     end
     def shinjuku
-        @station = Shopbranch.find_by_sql(['select * from shopbranches where station = "新宿"'])
+        @station = Shopbranch.find_by_sql(["select * from shopbranches where station = '新宿'"])
         @shopinfo = Shopinfo.all
     end
     def shibuya
-        @station = Shopbranch.find_by_sql(['select * from shopbranches where station = "渋谷"'])
+        @station = Shopbranch.find_by_sql(["select * from shopbranches where station = '渋谷'"])
         @shopinfo = Shopinfo.all
     end
     def ikebukuro
-        @station = Shopbranch.find_by_sql(['select * from shopbranches where station = "池袋"'])
+        @station = Shopbranch.find_by_sql(["select * from shopbranches where station = '池袋'"])
         @shopinfo = Shopinfo.all
     end
 
     def hokkaido
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "北海道"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '北海道'"])
         @shopinfo = Shopinfo.all
     end
     def tohoku
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "東北"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '東北'"])
         @shopinfo = Shopinfo.all
     end
     def kanto
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "関東"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '関東'"])
         @shopinfo = Shopinfo.all
     end
     def chubu
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "中部"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '中部'"])
         @shopinfo = Shopinfo.all
     end
     def kansai
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "関西"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '関西'"])
         @shopinfo = Shopinfo.all
     end
     def chugoku
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "中国"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '中国'"])
         @shopinfo = Shopinfo.all
     end
     def shikoku
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "四国"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '四国'"])
         @shopinfo = Shopinfo.all
     end
     def kyushu
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where area = "九州"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where area = '九州'"])
         @shopinfo = Shopinfo.all
     end
     def chiba
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where prefec = "千葉"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where prefec = '千葉'"])
         @shopinfo = Shopinfo.all
     end
     def kanagawa
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where prefec = "神奈川"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where prefec = '神奈川'"])
         @shopinfo = Shopinfo.all
     end
     def tochigi
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where prefec = "栃木"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where prefec = '栃木'"])
         @shopinfo = Shopinfo.all
     end
     def gunma
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where prefec = "群馬"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where prefec = '群馬'"])
         @shopinfo = Shopinfo.all
     end
     def ibaraki
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where prefec = "茨城"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where prefec = '茨城'"])
         @shopinfo = Shopinfo.all
     end
     def saitama
-        @area = Shopbranch.find_by_sql(['select * from shopbranches where prefec = "埼玉"'])
+        @area = Shopbranch.find_by_sql(["select * from shopbranches where prefec = '埼玉'"])
         @shopinfo = Shopinfo.all
     end
 
