eda96af9a11c198801dec772901fb157242b83b7
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2017-05-05 17:08:09 +0900

    install webpack
    configure webpack
    
    configure glob
    
    configure glob , const
    
    configure webpack
    
    update directory
    
    update package.json for webpack

---
+++ config/development/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const glob = require("glob");
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    styles: glob.sync('./src/javascripts/**/*.js'),
+  },
+  output: {
+    path: path.join(__dirname, "../../app/assets/javascripts"),
+    filename: '[name]-[hash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-2'
+      }
+    ]
+  }
+}
